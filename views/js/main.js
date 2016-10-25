bdafe9665cbe7f01e8307b339f3dcc37779b716a
Tejas <tejasdev1995@gmail.com>
2016-10-25 21:26:32 +0530

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -508,10 +508,8 @@ function updatePositions() {
   var items = document.getElementsByClassName('mover');
 
   var top = document.body.scrollTop / 1250;
-  var phase;
-
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(top + i % 5);
+    var phase = Math.sin((top + i) % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
